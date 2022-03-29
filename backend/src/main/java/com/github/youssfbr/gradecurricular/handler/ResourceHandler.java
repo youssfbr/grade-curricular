388ae7752d228e4a0aa6d9c52710aaa11a6ea791
Alisson Youssf <youssfbr@gmail.com>
2022-03-29 17:30:15 -0300

    Response Generico

--- backend/src/main/java/com/github/youssfbr/gradecurricular/handler/ResourceHandler.java
+++ backend/src/main/java/com/github/youssfbr/gradecurricular/handler/ResourceHandler.java
@@ -1,11 +1,8 @@
 package com.github.youssfbr.gradecurricular.handler;
 
 import com.github.youssfbr.gradecurricular.exception.MateriaException;
-import com.github.youssfbr.gradecurricular.exception.ErrorMapResponse;
-import com.github.youssfbr.gradecurricular.exception.ErrorMapResponse.ErrorMapResponseBuilder;
-import com.github.youssfbr.gradecurricular.model.ErrorResponse;
-import com.github.youssfbr.gradecurricular.model.ErrorResponse.ErrorResponseBuilder;
 
+import com.github.youssfbr.gradecurricular.model.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -21,7 +18,7 @@ import java.util.Map;
 public class ResourceHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorMapResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException m) {
+    public ResponseEntity<Response<Map<String, String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException m) {
 
         Map<String, String> erros = new HashMap<>();
 
@@ -30,23 +27,22 @@ public class ResourceHandler {
             String mensagem = erro.getDefaultMessage();
             erros.put(campo, mensagem);
         });
-        ErrorMapResponseBuilder errorMap = ErrorMapResponse.builder();
-        errorMap.errors(erros)
-                .httpStatus(HttpStatus.BAD_REQUEST.value())
-                .timeStamp(System.currentTimeMillis());
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMap.build());
+        Response<Map<String, String>> response = new Response<>();
+        response.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        response.setData(erros);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(MateriaException.class)
-    public ResponseEntity<ErrorResponse> handlerMateriaException(MateriaException m) {
+    public ResponseEntity<Response<String>> handlerMateriaException(MateriaException m) {
 
-        ErrorResponseBuilder erro = ErrorResponse.builder();
-        erro.httpStatus(m.getHttpStatus().value());
-        erro.mensagem(m.getMessage());
-        erro.timeStamp(System.currentTimeMillis());
+        Response<String> response = new Response<>();
+        response.setStatusCode(m.getHttpStatus().value());
+        response.setData(m.getMessage());
 
-        return ResponseEntity.status(m.getHttpStatus()).body(erro.build());
+        return ResponseEntity.status(m.getHttpStatus()).body(response);
     }
 
 }
--- backend/src/main/java/com/github/youssfbr/gradecurricular/model/Response.java
+++ backend/src/main/java/com/github/youssfbr/gradecurricular/model/Response.java
@@ -10,4 +10,9 @@ public class Response<T> extends RepresentationModel<Response<T>> {
 
     private int statusCode;
     private T data;
+    private long timeStamp;
+
+    public Response() {
+        timeStamp = System.currentTimeMillis();
+    }
 }
